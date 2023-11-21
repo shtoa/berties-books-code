6735e6806f365a2012e6d748fbe122a3d171dd64
Matvey <howtomatben@gmail.com>
2023-11-21 18:21:03 +0000

    Finished lab3 part 2

--- routes/main.js
+++ routes/main.js
@@ -12,7 +12,21 @@ module.exports = function(app, shopData) {
     });
     app.get('/search-result', function (req, res) {
         //searching in the database
-        res.send("You searched for: " + req.query.keyword);
+       
+        let sqlquery = "SELECT * FROM books where name like " + "'%" + req.query.keyword + "%'"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+                console.log(err)
+            }
+            let newData = Object.assign({}, shopData, {availableBooks:result, keyword: req.query.keyword});
+            //console.log(newData)
+            res.render("searchresult.ejs", newData)
+         });
+        
+        
+
     });
     app.get('/register', function (req,res) {
         res.render('register.ejs', shopData);                                                                     
@@ -24,11 +38,50 @@ module.exports = function(app, shopData) {
             if (err) {
                 res.redirect('./'); 
             }
-            res.send(result)
+
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            //console.log(newData)
+            res.render("list.ejs", newData)
          });
-    });                                                                                            
+         
+    });       
+    
+    app.get('/bargainbooks', function(req, res) {
+        let sqlquery = "SELECT * FROM books WHERE price < 20"; // query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            }
+
+            let newData = Object.assign({}, shopData, {availableBooks:result});
+            //console.log(newData)
+            res.render("bargainbooks.ejs", newData)
+         });
+         
+    });       
+
     app.post('/registered', function (req,res) {
         // saving data in database
         res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
     }); 
+    app.get('/addbook', function (req,res) {
+        res.render('addbook.ejs', shopData);                                                                     
+    });     
+    app.post('/bookadded', function (req,res) {
+        // saving data in database
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+          if (err) {
+            return console.error(err.message);
+          }
+          else {
+            res.send(' This book is added to database, name: '
+                      + req.body.name + ' price '+ req.body.price);
+          }
+        });
+  });    
+
 }
